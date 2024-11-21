36273ea09f81b1e0331f2622a39d5d5ef3adab31
songyb111-gachon <songyb111@gachon.ac.kr>
2024-11-21 22:32:41 +0900

    ewpojdasfklsnadfklsdf

--- open_manipulator_pick_and_place/src/open_manipulator_pick_and_place.cpp
+++ open_manipulator_pick_and_place/src/open_manipulator_pick_and_place.cpp
@@ -223,9 +223,9 @@ void OpenManipulatorPickandPlace::demoSequence()
     break;
     case 1: // initial pose
       joint_angle.push_back( 0.01);
-    joint_angle.push_back(-0.30);
+    joint_angle.push_back(-0.80);
     joint_angle.push_back( 0.00);
-    joint_angle.push_back( 1.90);
+    joint_angle.push_back( 2.40);
     setJointSpacePath(joint_name_, joint_angle, 2.0);
     demo_count_ ++;
     break;
