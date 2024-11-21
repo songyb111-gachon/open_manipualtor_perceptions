193e8cf37ee538ad175fe99c82bcd75764b78cfc
songyb111-gachon <songyb111@gachon.ac.kr>
2024-11-21 22:29:54 +0900

    ewpojdasfklsnadfklsdf

--- open_manipulator_pick_and_place/src/open_manipulator_pick_and_place.cpp
+++ open_manipulator_pick_and_place/src/open_manipulator_pick_and_place.cpp
@@ -551,8 +551,8 @@ void OpenManipulatorPickandPlace::demoSequence()
 
     // 수정된 위치 값
     kinematics_position.push_back(0.019); // X 좌표
-    kinematics_position.push_back(0.099); // Y 좌표
-    kinematics_position.push_back(0.099); // Z 좌표
+    kinematics_position.push_back(0.105); // Y 좌표
+    kinematics_position.push_back(0.105); // Z 좌표
 
     // 기존 오리엔테이션 값 유지
     kinematics_orientation.push_back(0.74); // w 값
